6bbeccb8f3021030781fce2b1f1b848033fd3dc8
Nunez <helen.s.nunez@intel.com>
2020-07-23 10:35:25 -0700

    testing Project html and map

--- Project/static/js/config.js
+++ Project/static/js/config.js
@@ -1,3 +1,3 @@
 // API key
 // Helen's
-const API_key = "pk.eyJ1IjoiaGVsZW5udW5leiIsImEiOiJja2N4czI2bXQwMnk3MnRsaWZqY2dveTV6In0.D1qMGnVSfZ9nc_FK_ClCeg";
+const API_KEY = "pk.eyJ1IjoiaGVsZW5udW5leiIsImEiOiJja2N4czI2bXQwMnk3MnRsaWZqY2dveTV6In0.D1qMGnVSfZ9nc_FK_ClCeg";
