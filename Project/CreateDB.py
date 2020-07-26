e033fbc8d5cb2dd6187bb8499385a52a4c9dd049
lnicholsonmesm <61956735+lnicholsonmesm@users.noreply.github.com>
2020-07-26 03:05:45 -0700

    database created and filtered mostly

--- Project/CreateDB.py
+++ Project/CreateDB.py
@@ -12,22 +12,34 @@ import pandas as pd
 
 #________*_________*_________*_________*_________*_________*_________*_________*
 db_uri = "sqlite:///db.sqlite"
-engine = create_engine(db_uri, echo = True)
+#engine = create_engine(db_uri, echo = True)
+engine = create_engine(db_uri, echo = False)
 
 #________*_________*_________*_________*_________*_________*_________*_________*
 
-df = pd.read_csv("data/S2802.csv", na_values = ["-"])
-df = df.astype({"fips_id": "object"})
-dt = df.dtypes[1:20]
-print(f'data types of top 20 columns: {dt}')
+census = pd.read_csv("data/S2802.csv", na_values = ["-"])
+census = census.astype({"fips_id": "object"})
+census["county_name"] = [x.replace(" ", "") for x in census["county_name"]]
+census["state"] = [x.replace(" ", "") for x in census["state"]]
+dt = census.dtypes[1:10] 
+print(f'data types of top 10 columns: {dt}')
 
 #________*_________*_________*_________*_________*_________*_________*_________*
 #Get LatLon
 latlon = pd.read_csv("data/fips_latlon.csv")
 
-#Merge on fips_code
-newdf = df.merge(latlon, how="inner", left_on=["id","fips_id"], right_on=["GEO_ID","StateTractCode"])
+#________*_________*_________*_________*_________*_________*_________*_________*
+#Get Region
+region = pd.read_csv("data/region.csv")
+dt = region.dtypes
+print(f'region data types are: {dt}')
+#________*_________*_________*_________*_________*_________*_________*_________*
+#Merge Region on County
+df_reg = census.merge(region, how="inner", left_on="county_name", right_on="CountyName")
 
+#Merge Coordinates on fips_code
+census_df = df_reg.merge(latlon, how="inner", left_on=["id","fips_id"], right_on=["GEO_ID","StateTractCode"])
+print(census_df)
 #________*_________*_________*_________*_________*_________*_________*_________*
 #Get FCC
 fcc = pd.read_csv("data/fcc_data.csv") #, na_values = ["-"]
@@ -41,12 +53,11 @@ print(f"fcc dtypes: {dt}")
 
 #________*_________*_________*_________*_________*_________*_________*_________*
 # To SQL census
-newdf.to_sql("s2802", con=engine, if_exists="replace", index_label=None)
+census_df.to_sql("census", con=engine, if_exists="replace", index_label=None)
 
 #To SQL fcc
 fcc_df.to_sql("fcc", con=engine, if_exists="replace", index_label=None)
 
-
 #Test
-engine.execute("SELECT * FROM s2802 LIMIT 2").fetchall()
-engine.execute("SELECT * FROM fcc LIMIT 2").fetchall()
+print(engine.execute("SELECT * FROM census LIMIT 2").fetchall())
+print(engine.execute("SELECT * FROM fcc LIMIT 2").fetchall())
