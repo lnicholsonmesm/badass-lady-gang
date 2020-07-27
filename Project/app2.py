815e48aad10014b839f20337ef071bdaef99355c
lnicholsonmesm <61956735+lnicholsonmesm@users.noreply.github.com>
2020-07-26 18:42:01 -0700

    commit now

---
+++ Project/app2.py
@@ -0,0 +1,317 @@
+# SQLAlchemy
+from sqlalchemy import create_engine
+
+# Imports the methods needed to abstract classes into tables
+from sqlalchemy.ext.declarative import declarative_base
+
+# Allow us to declare column types
+from sqlalchemy import Table, Column, Integer, String, Float, MetaData
+
+#pandas for data manipulation and export
+import pandas as pd
+
+#________*_________*_________*_________*_________*_________*_________*_________*
+db_uri = "sqlite:///db.sqlite"
+#engine = create_engine(db_uri, echo = True)
+engine = create_engine(db_uri, echo = False)
+
+#________*_________*_________*_________*_________*_________*_________*_________*
+
+census = pd.read_csv("data/S2802.csv", na_values = ["-"])
+census = census.astype({"fips_id": "object"})
+census["county_name"] = [x.replace(" ", "") for x in census["county_name"]]
+census["state"] = [x.replace(" ", "") for x in census["state"]]
+dt = census.dtypes[1:10] 
+print(f'data types of top 10 columns: {dt}')
+
+#________*_________*_________*_________*_________*_________*_________*_________*
+#Get LatLon
+latlon = pd.read_csv("data/fips_latlon.csv")
+
+#________*_________*_________*_________*_________*_________*_________*_________*
+#Get Region
+region = pd.read_csv("data/region.csv")
+dt = region.dtypes
+print(f'region data types are: {dt}')
+
+missingcol = pd.read_csv("data/missingcol.csv")
+missingcol
+pd.merge(census, region, left_on="county_name", right_on="CountyName")
+
+#________*_________*_________*_________*_________*_________*_________*_________*
+#Merge missing column, on fips_code
+df_col = census.merge(missingcol, how="inner", on="fips_id")
+
+#Merge Region on County
+df_reg = df_col.merge(region, how="inner", left_on="county_name", right_on="CountyName")
+
+#Merge Coordinates on fips_code
+census_df = df_reg.merge(latlon, how="inner", left_on=["id","fips_id"], right_on=["GEO_ID","StateTractCode"])
+census_df.iloc[0,10:50]
+#census_df.PE_Computer_Broadband_Household
+print(census_df.E_Computer_Broadband_Household)
+#________*_________*_________*_________*_________*_________*_________*_________*
+#Get FCC
+fcc = pd.read_csv("data/fcc_data.csv") #, na_values = ["-"]
+fcc = fcc.astype({"tract": "object"})
+
+#subset columns
+fcc_subset = fcc[["dbaname","hocofinal","stateabbr","blockcode", "tract", "techcode","consumer","maxaddown","maxadup"]]
+
+#fcc_df
+region_subset = census_df[["CountyName", "Region", "Latitude", "Longitude", "fips_id"]]
+fcc_df = fcc_subset.merge(region_subset, how="left", left_on="tract", right_on="fips_id")
+fcc_df
+
+fcc_df
+dt = fcc_df.dtypes
+print(f"fcc dtypes: {dt}")
+
+#________*_________*_________*_________*_________*_________*_________*_________*
+# To SQL census
+census_df.to_sql("census", con=engine, if_exists="replace", index_label=None)
+
+#To SQL fcc
+fcc_df.to_sql("fcc", con=engine, if_exists="replace", index_label=None)
+
+#Test
+print(engine.execute("SELECT * FROM census LIMIT 2").fetchall())
+print(engine.execute("SELECT * FROM fcc LIMIT 2").fetchall())
+
+
+# import necessary libraries
+import sqlalchemy
+from sqlalchemy import create_engine
+from flask import Flask, render_template, jsonify, request, redirect
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///db.sqlite")
+
+@app.route("/")
+def home():
+    #return render_template("index.html")
+    return render_template("laura.html")
+
+@app.route("//")
+def apitest():
+    return render_template("laura.html")
+
+@app.route("/northerncoast/")
+def apitesting():
+    return render_template("northerncoast.html")
+
+
+@app.route("/fcc/<region>")
+def fcc(region):
+    query = "SELECT * FROM fcc" #Where Region = '{region}' -- need to add to df
+    results = engine.execute(race_query).fetchall()
+    dictionary = {"data": results}
+    return jsonify(dictionary)
+  
+
+#load race query
+from race_query import race_query, race_columns
+
+@app.route("/treemap/<region>/")
+def treemap(region):
+    census = []
+    results = engine.execute(race_query).fetchall()
+    df = pd.DataFrame(results)
+    df.columns = race_columns
+    data_frame = df.groupby("Region").sum()
+
+    # E_Computer_Broadband_Households
+    # E_Computer_NoInternet_Households
+    #NoComputer_Households
+    
+    arrays = [["Race", None]
+        ,["White","Race"]
+        ,["With Computer, White", "White"]
+        ,["No Computer, White", "White"]
+        ,["With Internet", "With Computer, White"]
+        ,["American Indian or Alaska Native","Race"]
+        ,["With Computer, Indigenous", "American Indian or Alaska Native"]
+        ,["No Computer, Indigenous", "American Indian or Alaska Native"]
+        ,["With Internet", "With Computer, Indigenous"]
+        ,["Asian","Race"]
+        ,["With Computer, Asian", "Asian"]
+        ,["No Computer, Asian", "Asian"]
+        ,["With Internet", "With Computer, Asian"]
+        ,["Native Hawaiian/Other Pacific Islander", "Race"]
+        ,["Other", "Race"]
+        ,["Two or more Races", "Race"]]
+    for x in range(length(arrays)):
+        arrays[x].append(data_frame.iloc[0,x])
+    js = jsonify(arrays)
+    return js
+    #{"latitude": row[0]}
+    #census.append(mydict)
+        #if upper(enter_race_or_ethnicity) == "RACE" then:
+        #where = "race"
+    #OnlyWhite = []
+    #census = {
+    #    ""
+    #}
+    #mapdata =jsonify(census)
+    #return mapdata
+
+
+@app.route("/map/<region>")
+def map(region):
+    census = []
+    mapquery = f"select Latitude, Longitude, Region, county_name, fips_id, PE_Computer_Broadband_Household, E_Computer_Broadband_Household FROM census"
+    # where Region = '{region}"
+    results = engine.execute(mapquery)
+    
+    for row in results:
+        mydict = {
+            "latitude": row[0], 
+            "longitude": row[1],
+            "region": row[2],
+            "county": row[3],
+            "tract": row[4],
+            "pct_has_computer_and_broadband": row[5],
+            "has_computer_and_broadband": row[6]
+            } #{"latitude": row[0]}
+        census.append(mydict)
+       
+    return jsonify(census)
+    #return redirect("/", code=302)
+
+@app.route("/region2")
+def map2():
+    query = "SELECT Latitude, Longitude, Region, county_name, fips_id, PE_Computer_Broadband_Household, E_Computer_Broadband_Household FROM census"
+    results = engine.execute(query)
+    mapdata = []
+    for row in results:
+        datadict = {
+            "latitude": row[0],
+            "longitude": row[1],
+            "region": row[2],
+            "county": row[3],
+            "fips_id": row[4],
+            "pct_ComputerAndBroadband": row[5],
+            "ComputerAndBroadband": row[6]
+        }
+        mapdata.append(datadict)
+    return jsonify(mapdata)
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+# # Remove tracking modifications
+# # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db = SQLAlchemy(app)
+
+# Pet = create_classes(db)
+
+# # create route that renders index.html template
+# @app.route("/")
+# def home():
+#     session = Session(engine)
+#     return render_template("index.html")
+
+
+# # Query the database and send the jsonified results
+# """
+# @app.route("/send", methods=["GET", "POST"])
+# def send():
+#     if request.method == "POST":
+#         name = request.form["petName"]
+#         lat = request.form["petLat"]
+#         lon = request.form["petLon"]
+
+#         pet = Pet(name=name, lat=lat, lon=lon)
+#         db.session.add(pet)
+#         db.session.commit()
+#         return redirect("/", code=302)
+
+#     return render_template("form.html")
+#     """
+
+
+# @app.route("/api") ----- can we merge into a single app.route?
+# def pals():
+#     results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
+
+#     hover_text = [result[0] for result in results]
+#     lat = [result[1] for result in results]
+#     lon = [result[2] for result in results]
+
+#     pet_data = [{
+#         "type": "scattergeo",
+#         "locationmode": "USA-states",
+#         "lat": lat,
+#         "lon": lon,
+#         "text": hover_text,
+#         "hoverinfo": "text",
+#         "marker": {
+#             "size": 50,
+#             "line": {
+#                 "color": "rgb(8,8,8)",
+#                 "width": 1
+#             },
+#         }
+#     }]
+
+#     return jsonify(pet_data)
+
+'''
+
+    White = []
+    AmIndAlasNat = []
+    Black = []
+    Asian = []
+    HawaiianPacific = []
+    OneRaceOther = []
+    TwoPlusRaces = []
+    CompInt = []
+    for array in results:
+        for x in range(len(array))
+        w= row[0]
+        n= row[1]
+        b= row[2]
+        a= row[3]
+        h= row[4]
+        o= row[5]
+        t= row[6]
+        _ci = row[7]
+        wci = row[8]
+        nci = row[9]
+        bci = row[10]
+        aci = row[11]
+        hci = row[12]
+        oci = row[13]
+        tci = row[14]
+        _cni= row[15]
+        wcni = row[16]
+        ncni = row[17]
+        bcni = row[18]
+        acni = row[19]
+        hcni = row[20]
+        ocni = row[21]
+        tcni = row[22]
+        hl_any = row[23]
+        w_not_hl = row[24]
+        hl_any_ci = row[25]
+        w_not_hl_ci = row[26]
+        ncw = row[27]
+        ncn = row[28]
+        ncb = row[29]
+        nca = row[30]
+        nc_hl_any = row[31]
+        nc_w_not_hl = row[32]
+'''
+
+if __name__ == "__main__":
+    app.run()
---
+++ Project/static/js/logi2c.js
@@ -0,0 +1,142 @@
+//Get data from Flask App API call
+function optionChanged(val) {
+    var subjectID = val;
+    console.log(subjectID);
+    //plotGraph(subjectID);
+};
+/*
+var selector = d3.select("#selDataset");
+console.log()
+d3.select(this)
+    .selectAll("dropdown-item")
+    .filter(function (d, i) {
+        console.log(this.selected);
+    }); */
+var selector = d3.select("#getRegion")
+//var selector = d3.select("#region").node().value;
+console.log(selector);
+d3.selectAll("#region").on("change", console.log(selector));
+var test = d3.select("#navbarDropdown").node().value;
+d3.selectAll("#navbarDropdown").on("change", console.log(test));
+
+
+d3.json("/map/region").then((response) => {
+    console.log(response);
+    //Define base map centered in Portland
+    var myMap = L.map("map-id", {
+        center: [45.52, -122.67],
+        zoom: 7,
+    });
+
+    //Add Tile Layer from Mapbox
+    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
+    }).addTo(myMap);
+
+    //Get Maxvalue for intensity using response and correct column "household"
+    householdData = response.map(track => track.has_computer_and_broadband)
+    maxValue = Math.max(has_computer_and_broadband)
+
+    //Function to get intensity based on number of housholds
+    //var max = 3000
+    function getIntensity(rawValue, maxValue) {
+        var intensity = rawValue / maxValue
+        return intensity
+    }
+    //getIntensity(200,max)
+
+    //Get an array of the parametres that the heat layer requires: latitude, longitude, householdcount
+    var getHeatInfo = response.map(track => { return [track.latitude, track.longitude, getIntensity(track.has_computer_and_broadband, maxValue)] })
+    //Add Heat Layer to map using the getHeatInfo array
+    var heat = L.heatLayer([
+        //[45.52, -122.67, 500], lat, lng, intensity
+        getHeatInfo], { radius: 35 }).addTo(myMap);
+    //give it a gradient
+})
+
+
+
+
+
+      // var tractData = "https://raw.githubusercontent.com/loganpowell/census-geojson/master/GeoJSON/500k/2010/41/tract.json";
+      // // var geoData1 = "https://opendata.arcgis.com/datasets/e3320c8931b144aaa71eebec059ac5cd_0.geojson"
+      // //  //var geoData2 = "https://raw.githubusercontent.com/loganpowell/census-geojson/master/GeoJSON/500k/2018/41/tract.json";
+      // //  //Get data
+      // d3.json(tractData, function(data) {
+      //         //console.log (data)
+      //         L.geoJSON(data).addTo(myMap)
+      // //         //createFeatures(data.features);
+      //     });
+
+
+  //intensity = households
+  //find the max intesity, set it to 1
+
+
+
+
+  //call the route in the url
+
+
+
+
+  // var circle = L.circle([45.52, -122.67],{
+  //         color: 'red',
+  //         fillColor: '#f03',
+  //         fillOpacity: 0.5,
+  //         radius: 5000
+  //     }).addTo(myMap);
+
+  // async function getData(){
+  //         const response = await fetch('/api');
+  //         const data = await response.json()
+  //         for (item of data){
+  //                 const marker = L.marker([item.lat, item.lon]).addTo(myMap);
+  //                 const txt = `This is the information ${item.lat}; population is ${item.population}`;
+  //                 marker.bindPopup(txt)
+  //         }
+  //         console.log(data)
+  // }
+
+  //heatmap #housholds that have internet access
+
+
+  // To include the plugin, just use leaflet-heat.js from the dist folder:
+  // <script src="leaflet-heat.js"></script>
+
+
+
+  // function buildPlot() {
+  //   /* data route */
+  //   const url = "/api/pals";
+  //   d3.json(url).then(function(response) {
+  //     console.log(response);
+  //     const data = response;
+  //     const layout = {
+  //       scope: "usa",
+  //       title: "Pet Pals",
+  //       showlegend: false,
+  //       height: 600,
+  //             // width: 980,
+  //       geo: {
+  //         scope: "usa",
+  //         projection: {
+  //           type: "albers usa"
+  //         },
+  //         showland: true,
+  //         landcolor: "rgb(217, 217, 217)",
+  //         subunitwidth: 1,
+  //         countrywidth: 1,
+  //         subunitcolor: "rgb(255,255,255)",
+  //         countrycolor: "rgb(255,255,255)"
+  //       }
+  //     };
+  //     Plotly.newPlot("plot", data, layout);
+  //   });
+  // }
+  // buildPlot();
